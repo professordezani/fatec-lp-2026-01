a452f0a269d5df6e5b793621161aa2a6df160c8b
Dezani <henrique.dezani@fatecriopreto.edu.br.local>
2026-03-16 16:14:58 -0300

    Adiciona exercícios extras

---
+++ ex_extra01.py
@@ -0,0 +1,3 @@
+# Crie um programa que solicite ao usuário 
+# uma palavra, em seguida conte e mostre quantas
+# vogais existem na palavra digitada.
---
+++ ex_extra02.py
@@ -0,0 +1,4 @@
+# Crie um programa que solicite ao usuário um número
+# inteiro N e calcule a soma de todos os números pares
+# de 1 até N (exlusivo, ou seja, N não deve ser
+# considerado na soma)
